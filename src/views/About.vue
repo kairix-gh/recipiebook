9e2be2d4014ec8231aca9750bba58305acc7bb64
Kairix <kairix@teameligere.com>
2021-09-01 17:48:55 -0600

    - Some more about stuff

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
     <div>
         <h1 class="text-lg font-medium italic mb-6 border-b">About this Recipie Book</h1>
         <p class="mb-4">This is a collection recipies I've saved over the years - some from friends, some from the internet, and some I made up myself. Originally I was keeping these recipies saved in crazy formats like .txt files, OneNote pages, excel sheets<span class="text-red-500 font-medium px-1">(!)</span>, and all sorts of crazy stuff. It was obviously very unmaintainable and after I started meal prepping it became exceedingly tedious to put together weekly shopping lists, and I would always step 15 or 20 minutes trying to figure out where each recipie was saved.</p>
-        <p>Eventually I decided that I would just sit down and take the time to built a simple online solution that I could use to save recipies that I wanted to keep, and also provide a super convenient way to make weekly shopping lists. Thus, this project was born.</p>
+        <p class="mb-4">Eventually I decided that I would just sit down and take the time to built a simple online solution that I could use to save recipies that I wanted to keep, and also provide a super convenient way to make weekly shopping lists. Thus, this project was born.</p>
+        <p>The main goal was for this site to have no images, ads, or anything that could impede loading the content - save for the calls to the API to retrieve data from the database. I also wanted the option to allow other folks close to me to add users if that became a thing, so asking someone who is not technically inclined to create and upload markdown files seemed like an unwise decision.</p>
     </div>
 </template>
