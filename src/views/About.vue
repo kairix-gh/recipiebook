c6ea7a5fefb1da5f60f459e5cd30328212de0bfb
Kairix <kairix@teameligere.com>
2021-09-01 17:47:16 -0600

    - Add some text about this project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <h1 class="text-lg font-medium italic mb-6 border-b">About this Recipie Book</h1>
+        <p class="mb-4">This is a collection recipies I've saved over the years - some from friends, some from the internet, and some I made up myself. Originally I was keeping these recipies saved in crazy formats like .txt files, OneNote pages, excel sheets<span class="text-red-500 font-medium px-1">(!)</span>, and all sorts of crazy stuff. It was obviously very unmaintainable and after I started meal prepping it became exceedingly tedious to put together weekly shopping lists, and I would always step 15 or 20 minutes trying to figure out where each recipie was saved.</p>
+        <p>Eventually I decided that I would just sit down and take the time to built a simple online solution that I could use to save recipies that I wanted to keep, and also provide a super convenient way to make weekly shopping lists. Thus, this project was born.</p>
+    </div>
 </template>
